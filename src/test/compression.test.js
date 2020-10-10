21967b59b3d2ec317fa17423f1ac585c88e89f6d
nulldev <me@nulldev.xyz>
2020-10-10 16:22:06 -0400

    Fix bad order of operations in compressToUTF16Async

--- src/test/compression.test.js
+++ src/test/compression.test.js
@@ -1,5 +1,5 @@
 import {should} from 'chai'
-import {compressAsync, decompressAsync, sortAsync} from '../component'
+import {compressAsync, decompressAsync, compressToUTF16Async, decompressFromUTF16Async, sortAsync} from '../component'
 
 should()
 const INPUT_ARRAY = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
@@ -11,5 +11,11 @@ describe('compression', function () {
         let output = await decompressAsync(result)
         output.should.eq('abcde')
         result.should.eq('ↂテɠꙀ')
+    })
+    it('should compress a string to UTF 16', async function () {
+        let result = await compressToUTF16Async('abcde')
+        let output = await decompressFromUTF16Async(result)
+        output.should.eq('abcde')
+        result.should.eq('ს౑䁬઄  ')
     })
 })
